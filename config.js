a990ddcde5392d87eef7a7287b2d7cf1833afc27
depannn <depstoreku@gmail.com>
2025-12-25 11:05:47 +0700

    Upload: config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  token: '8441757941:AAE0pizKFheEjbrcRltQMpnhFHoce7w-Neo', //TAKE FROM @BotFather
+  ownerId: 8412273544, // TAKE FROM @CekIDTelegram_bot
+  botMode: 'self', //CHANGE USING COMMAND .setmode
+  botVersion: '1.0.1', //CHANGE VERSION ACCORDING TO SYSTEM
+  apiProvider: 'https://zelapioffciall.koyeb.app', //CHANGE TO YOUR API PROVIDER 
+  botName: 'Soraa MD'
+};
